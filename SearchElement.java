15088978319a0114c12ea0dae3d6fc581646f2dd
AromalAnill <aaromalanill@gmail.com>
2026-02-18 13:56:53 +0530

    Add files via upload

---
+++ SearchElement.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SearchElement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Taking array input from user
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the element to search: ");
+        int element = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
